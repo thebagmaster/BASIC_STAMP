482fa723f90288a85697267942c9334f44205e59
Daniel Nichols <thebagmaster@gmail.com>
2014-09-20 21:44:03 -0500

    First Commit

---
+++ git.bat
@@ -0,0 +1,8 @@
+git.exe init
+git.exe add .
+git.exe commit -m "First Commit"
+git.exe remote add origin https://github.com/thebagmaster/BASIC_STAMP.git
+git.exe remote pull https://github.com/thebagmaster/BASIC_STAMP.git
+git.exe push origin master
+echo Press Enter...
+read
