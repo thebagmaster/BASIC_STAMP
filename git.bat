1198be31581418f036df696c71473c2f5ce53f9d
Daniel Nichols <thebagmaster@gmail.com>
2014-09-20 21:45:53 -0500

    First Commit

--- git.bat
+++ git.bat
@@ -2,7 +2,7 @@ git.exe init
 git.exe add .
 git.exe commit -m "First Commit"
 git.exe remote add origin https://github.com/thebagmaster/BASIC_STAMP.git
-git.exe remote pull https://github.com/thebagmaster/BASIC_STAMP.git
+git.exe pull https://github.com/thebagmaster/BASIC_STAMP.git
 git.exe push origin master
 echo Press Enter...
 pause
